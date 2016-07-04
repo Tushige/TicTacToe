ab10e105c30278e14e14bd651865c41193fb508f
Tushig Ochirkhuyag <Tushig@Tushigs-MacBook-Pro.local>
2016-07-04 17:39:09 -0500

    basic functionality is done

--- tic.js
+++ tic.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
      * user_pick choice of X or O
      */
     var mark = {"AI":"", "Player":"" };
+
     /*
      * 0: AI
      * 1: player
      */
     var playerTurn = 0;
-    var gameOver = false;
+    var move_counter = 0;
+
     /* BOOl
     * true: game in progress
     * false: game not in progress
@@ -20,10 +22,16 @@ $(document).ready(function() {
     var winnerDlg = $("#winnerDialog");
     var Play = $("#opener-btn");
     var cells = $(".cell");
+    var player = document.getElementById("player");
+    var player_score = document.getElementById("player_score");
+    var AI = document.getElementById("AI");
+    var AI_score = document.getElementById("AI_score");
+    var tie_score = document.getElementById("tie_score");
 
     //AI random number generator params
     var max = 9;
     var min = 0;
+
     /*
      * toggles player
      */
@@ -34,6 +42,8 @@ $(document).ready(function() {
         if(playerTurn === 0)
             AIMove();
     }
+
+    /*computer makes a move*/
     function AIMove() {
         if(inGame == false)
             return;
@@ -49,6 +59,8 @@ $(document).ready(function() {
         hasWinner(mark.AI);
         endTurn();
     }
+
+    /*display winner and get ready for next game*/
     function setupEndWidget(winner) {
         winnerDlg.dialog({
             autoOpen: true,
@@ -63,6 +75,14 @@ $(document).ready(function() {
                     for(var i in cells) {
                         cells[i].textContent = "";
                     }
+                    player.innerText = "Player";
+                    AI.innerText = "AI";
+                    //reset game Variables
+                    inGame = false;
+                    mark.player = "";
+                    mark.AI = "";
+                    playerTurn = 0;
+                    move_counter = 0;
                     $(this).dialog("close");
                 }
             },
@@ -72,18 +92,36 @@ $(document).ready(function() {
             }
         });
     }
-    // do clean up
+
+    // do clean up for next game
     function gameOverHandler(winner) {
-        setupEndWidget(Object.keys(mark)[playerTurn]);
-        //reset game Variables
-        inGame = false;
-        GameOver = true;
-        mark.player = "";
-        mark.AI = "";
-        playerTurn = 0;
+        var scoreTemp;
+        //if there is a winner
+        if(winner != "no one") {
+            //player won!
+            if(playerTurn == 1) {
+                scoreTemp = player_score.innerText;
+                player_score.innerText = eval(scoreTemp+"+1");
+            }
+            //AI won!
+            else {
+                scoreTemp = AI_score.innerText;
+                AI_score.innerText = eval(scoreTemp+"+1");
+            }
+            setupEndWidget(Object.keys(mark)[playerTurn]);
+        }
+        //tie! 
+        else {
+            setupEndWidget("no one");
+            scoreTemp = tie_score.innerText;
+            tie_score.innerText = eval(scoreTemp + "+1");
+        }
     }
 
+    /*check if we have a winner*/
     function hasWinner(pick) {
+        move_counter++;
+        //check if there is a winner
         if( (cells[0].textContent=== pick && cells[1].textContent === pick && cells[2].textContent === pick) ||
             (cells[0].textContent === pick && cells[3].textContent === pick && cells[6].textContent === pick) ||
             (cells[0].textContent === pick && cells[4].textContent === pick && cells[8].textContent === pick) ||
@@ -94,9 +132,15 @@ $(document).ready(function() {
             (cells[2].textContent === pick && cells[4].textContent === pick && cells[6].textContent === pick)) {
                 gameOverHandler(pick);
         }
+        //tie!
+        else if(move_counter==9){
+            gameOverHandler("no one");  
+        }
     }
+    
     //setup the window that asks the user_pick to choose 'X' vs 'O'
     function setupStartWidget() {
+        console.log("114")
         playDlg.dialog({
         autoOpen: false,
         hide: "puff",
@@ -104,18 +148,24 @@ $(document).ready(function() {
         height: 200,
         modal: true,
         title: "MAKE YOUR CHOICE!",
-        buttons: {
+         buttons: {
             O: function() {
                   mark.player = "O";
                   mark.AI = "X";
+                  player.innerText += "(O)";
+                  AI.innerText += "(X)";
                   inGame = true;
+                  move_counter = 0;
                   AIMove();
                   $(this).dialog("close");
             },
             X: function() {
                   mark.player = "X";
                   mark.AI = "O";
+                  player.innerText += "(X)";
+                  AI.innerText += "(O)";
                   inGame = true;
+                  move_counter = 0;
                   AIMove();
                   $(this).dialog("close");
             }
@@ -136,8 +186,7 @@ $(document).ready(function() {
 
         /*when user_pick makes a move*/
         cells.click(function() {
-            //if(inGame) && playerTurn===1) {
-            if(inGame) {
+            if(inGame && playerTurn===1) {
                 if(this.textContent === "") {
                     //make move and end turn
                     this.textContent = mark.player;
